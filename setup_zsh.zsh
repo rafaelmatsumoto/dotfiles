edff25642e3ba15055eb1a9dae698c0224570c85
rafaelmatsumoto <37717679+rafaelmatsumoto@users.noreply.github.com>
2022-07-01 15:27:30 -0300

    feat: throw stdout from setup_zsh to /dev/null

--- setup_zsh.zsh
+++ setup_zsh.zsh
@@ -3,7 +3,7 @@
 echo "\n<<< Starting ZSH Setup >>>\n"
 
 echo "Enter superuser (sudo) password to edit /etc/shells"
-echo '/opt/homebrew/bin/zsh' | sudo tee -a '/etc/shells'
+echo '/opt/homebrew/bin/zsh' | sudo tee -a '/etc/shells' >/dev/null
 
 echo "Enter user password to change login shell"
 chsh -s '/opt/homebrew/bin/zsh'		
