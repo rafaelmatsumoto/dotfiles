a7b77759a621dd6ce4098bb5bd5ecc49116d6557
rafaelmatsumoto <37717679+rafaelmatsumoto@users.noreply.github.com>
2022-07-01 15:46:24 -0300

    feat: symlink sh to zsh

--- setup_zsh.zsh
+++ setup_zsh.zsh
@@ -2,9 +2,24 @@
 
 echo "\n<<< Starting ZSH Setup >>>\n"
 
-echo "Enter superuser (sudo) password to edit /etc/shells"
-echo '/opt/homebrew/bin/zsh' | sudo tee -a '/etc/shells' >/dev/null
+if grep -Fxq '/opt/homebrew/bin/zsh' '/etc/shells'; then
+	echo '/usr/local/bin/zsh already exists in /etc/shells'
+else
+	echo "Enter superuser (sudo) password to edit /etc/shells"
+	echo '/opt/homebrew/bin/zsh' | sudo tee -a '/etc/shells' >/dev/null
+fi
 
-echo "Enter user password to change login shell"
-chsh -s '/opt/homebrew/bin/zsh'		
+if [ "$SHELL" = '/opt/homebrew/bin/zsh' ]; then
+	echo '$SHELL is already /opt/homebrew/bin/zsh'
+else
+	echo "Enter user password to change login shell"
+	chsh -s '/opt/homebrew/bin/zsh'		
+fi
+
+if sh --version | grep -q zsh; then
+	echo '/private/var/select/sh already linked to /bin/zsh, even though brew zsh would be prefered'
+else
+	echo "Enter superuser (sudo) password to symlink sh to zsh"
+	sudo ln -sfv /bin/zsh /private/var/select/sh
+fi
 
