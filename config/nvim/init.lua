f220235790bfc40ce15c8c754fc0cb8f43ad677b
rafaelmatsumoto <37717679+rafaelmatsumoto@users.noreply.github.com>
2023-06-29 17:45:40 -0300

    feat: add initial tmux configuration

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -203,6 +203,7 @@ require('lazy').setup({
   --
   --    For additional information see: https://github.com/folke/lazy.nvim#-structuring-your-plugins
   -- { import = 'custom.plugins' },
+  {'akinsho/toggleterm.nvim', version = "*", config = true}
 }, {})
 
 -- [[ Setting options ]]
